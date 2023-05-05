0059c6ac0b2ed5c6fb3ee20f5d6ee757332bbcd7
Elena Chernousova <lenochka@Elenas-MacBook-Air.local>
2023-05-05 12:31:55 -0700

    comment delete api

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -14,7 +14,25 @@ router.post('/', async (req, res) => {
     if (newComment) {
     res.status(200).json({ message: "Comment added!"});
     } else {
-      res.status(400).json({ message: "insafficient data" })
+      res.status(404).json({ message: "insafficient data" });
+    }
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//delete one comment
+router.delete('/:commentId', async (req, res) => {
+  try {
+    const deleteComment = await Comment.destroy({
+      where: {
+        id: req.params.commentId,
+      },
+    });
+    if (deleteComment) {
+      res.status(200).json({ message: "Comment Deleted!"});
+    } else {
+      res.status(404).json({ message: "No comment with this ID"});
     }
   } catch (err) {
     res.status(400).json(err);
