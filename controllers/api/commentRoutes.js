b9227a9819a1a45e4557e76566423bf421e21262
Elena Chernousova <lenochka@Elenas-MacBook-Air.local>
2023-05-05 12:09:28 -0700

    new comment adding api

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -3,18 +3,24 @@ const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 //create NEW comment
-router.post('/:id', withAuth, async (req, res) => {
+router.post('/', async (req, res) => {
   try {
-    const newComment = await Comment.create({
-      ...req.body,
-      post_id: req.params.id,
-      user_id: req.session.user_id,
-    });
-
-    res.status(200).json(newComment);
+    const newComment = await Comment.create(req.body);
+    //{
+    //   ...req.body,
+    //   post_id: req.params.id,
+    //   user_id: req.session.user_id,
+    // });
+    if (newComment) {
+    res.status(200).json({ message: "Comment added!"});
+    } else {
+      res.status(400).json({ message: "insafficient data" })
+    }
   } catch (err) {
     res.status(400).json(err);
   }
 });
+
+
 
 module.exports = router;
